ac1d264d7113be3da627502081b8b95e9ece0cf4
Victor Jared Onato <jakewolfman131@gmail.com>
2025-11-13 22:50:49 +0800

    Changed the word 'Remove' to '🗑️' to follow the Trello Checklist

--- src/js/cart.js
+++ src/js/cart.js
@@ -28,7 +28,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-  <button type="button" class="remove-button" data-id=${item.Id}>Remove</button>
+  <button type="button" class="remove-button" data-id=${item.Id}>🗑️</button>
 </li>`;
 
   return newItem;
