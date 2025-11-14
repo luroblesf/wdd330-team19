6f66b88cc20f124550c3167539a73e18004293f9
Victor Jared Onato <jakewolfman131@gmail.com>
2025-11-14 10:29:50 +0800

    Revert "Merge branch 'main' of https://github.com/luroblesf/wdd330-team19"
    This reverts commit 69f87f1e3bc6d1d1038ac78ed42c0360109ef68a, reversing
    changes made to f6b242861a74a3fa7d59351861dd94a5bd2e9b7b.

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,5 +1,3 @@
-const baseURL = import.meta.env.VITE_BASE_URL;
-
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -9,20 +7,18 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor() {
-    // this.category = category;
-    // this.path = `../public/json/${this.category}.json`;
+  constructor(category) {
+    this.category = category;
+    this.path = `../json/${this.category}.json`;
   }
-  async getData(category) {
-    const response = await fetch(`${baseURL}products/search/${category}`);
-    const data = await convertToJson(response);
-    return data.Result;
+  getData() {
+    return fetch(this.path)
+      .then(convertToJson)
+      .then((data) => data);
   }
   async findProductById(id) {
-    const response = await fetch(`${baseURL}product/${id}`);
-    const data = await convertToJson(response);
-    console.log(data.Result);
-    return data.Result;
+    const products = await this.getData();
+    return products.find((item) => item.Id === id);
   }
 }
 
--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,11 @@
-import { loadHeaderFooter, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-loadHeaderFooter();
+const productId = getParam("product");
 
 const dataSource = new ProductData("tents");
-const productID = getParam("product");
 
-const product = new ProductDetails(productID, dataSource);
+const product = new ProductDetails(productId, dataSource);
 
 product.init();
