24642eafa0878c19d27a774879fb3f46937fb891
luroblesf <rob23047@byui.edu>
2025-11-19 20:25:29 -0600

    fixed

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,5 +1,6 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import { CheckoutProcess } from "./CheckOutProcess.mjs";
+import CheckoutProcess from "./CheckOutProcess.mjs";
+
 
 loadHeaderFooter();
 
